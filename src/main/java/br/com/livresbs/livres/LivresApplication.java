28f640f9eced7739f97433e062a8e8f05681cf29
Edrei <edreifernandes@gmail.com>
2020-05-13 21:35:10 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/livresbs/livres/LivresApplication.java
@@ -0,0 +1,13 @@
+package br.com.livresbs.livres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/livresbs/livres/LivresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livresbs.livres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
