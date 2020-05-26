3feac9385604fe60c340dcf36c2ce6d4aee743a0
DanielHGimenez <danielhgimenez027@gmail.com>
2020-05-25 23:54:29 -0300

    Implementação de assinatura da rota que lista os produtos

--- backend/src/main/java/br/com/livresbs/livres/controller/ProdutoController.java
+++ backend/src/main/java/br/com/livresbs/livres/controller/ProdutoController.java
@@ -1,10 +1,24 @@
 package br.com.livresbs.livres.controller;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController("/produtos")
 public class ProdutoController {
 
- 
+    @GetMapping
+    public ProdutosDisponiveisDTO getProdutosDisponiveisVenda(
+            @Valid @NotNull(message = "Id do consumidor é obrigatório") @RequestHeader Long consumidorId,
+            @Valid @NotNull(message = "Número da pagina é obrigatório") @RequestParam Integer numeroPagina,
+            BindingResult bindingResult
+    ) {
+
+    }
 
 }
