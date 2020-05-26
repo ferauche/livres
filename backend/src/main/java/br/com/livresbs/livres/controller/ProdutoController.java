bf845d415e338d5523c4eeaf68abecf1657b53b3
Daniel Gimenez <dgimenez.ext@vr.com.br>
2020-05-25 23:07:35 -0300

    Criando ProdutoController

---
+++ backend/src/main/java/br/com/livresbs/livres/controller/ProdutoController.java
@@ -0,0 +1,10 @@
+package br.com.livresbs.livres.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/produtos")
+public class ProdutoController {
+
+   
+
+}
