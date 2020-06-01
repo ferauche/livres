a4b233eb6b0a701242e45779c16ad544c9a9b24b
Edrei <edreifernandes@gmail.com>
2020-06-01 14:07:26 -0300

    Montando página com components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <app-header></app-header>
+    <app-form></app-form>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header";
+import Form from "./components/Form";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-form": Form
+  }
+};
 </script>
 
 <style>
+.livres-primary {
+  background: #41b1e0;
+}
+
+.livres-segundary {
+  background: #c27351;
+}
 </style>
