c5c9c02544a510d5456fbc812c28f54a044b36e5
Edrei <edreifernandes@gmail.com>
2020-06-01 15:18:39 -0300

    Consumindo API placeholder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,11 +8,23 @@
 <script>
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Users from "./services/users";
 
 export default {
   components: {
     "app-header": Header,
     "app-form": Form
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    Users.listar().then(result => {
+      console.log(result.data);
+      this.users = result.data;
+    });
   }
 };
 </script>
---
+++ frontend/src/services/config.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const http = axios.create({
+  // Placeholder
+  baseURL: "https://jsonplaceholder.typicode.com/",
+});
