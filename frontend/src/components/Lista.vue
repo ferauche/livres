0a1e67683e9473409aa6c1bdee410cb0d3306231
Edrei <edreifernandes@gmail.com>
2020-06-04 14:42:52 -0300

    Consumindo nossa API e removendo Placeholder

--- frontend/src/components/Lista.vue
+++ frontend/src/components/Lista.vue
@@ -2,37 +2,36 @@
   <table class="container table col-lg-10">
     <thead class="thead">
       <tr>
-        <th scope="col">Id</th>
         <th scope="col">Name</th>
-        <th scope="col">Username</th>
-        <th scope="col">Email</th>
+        <th scope="col">Sobrenome</th>
+        <th scope="col">CPF</th>
+        <th scope="col">Pré-comunidade</th>
       </tr>
     </thead>
 
     <tbody>
-      <tr v-for="user in users" :key="user.id">
-        <th scope="row">{{ user.id }}</th>
-        <td>{{ user.name }}</td>
-        <td>{{ user.username }}</td>
-        <td>{{ user.email }}</td>
+      <tr v-for="consumidor in consumidores" :key="consumidor.cpf">
+        <td>{{ consumidor.nome }}</td>
+        <td>{{ consumidor.sobrenome }}</td>
+        <td>{{ consumidor.cpf }}</td>
+        <td>{{ consumidor.precomunidade }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-import Users from "../services/users";
+import Consumidores from "../services/consumidores";
 
 export default {
   data() {
     return {
-      users: []
+      consumidores: []
     };
   },
   mounted() {
-    Users.listar().then(result => {
-      console.log(result.data);
-      this.users = result.data;
+    Consumidores.listar().then(result => {
+      this.consumidores = result.data;
     });
   }
 };
