020e0163e92ff8d83b5ec53fa372e54c8185206e
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-05 21:11:05 -0300

    movimentação de rotas de carrinho e pedido para novo endereço

--- backend/src/main/java/br/com/livresbs/livres/controller/LojaController.java
+++ backend/src/main/java/br/com/livresbs/livres/controller/LojaController.java
@@ -1,10 +1,16 @@
 package br.com.livresbs.livres.controller;
 
+import br.com.livresbs.livres.dto.CarrinhoDTO;
+import br.com.livresbs.livres.dto.CheckoutDTO;
+import br.com.livresbs.livres.dto.ProdutoCarrinhoDTO;
 import br.com.livresbs.livres.dto.ProdutosDisponiveisDTO;
+import br.com.livresbs.livres.service.CarrinhoService;
 import br.com.livresbs.livres.service.LojaService;
+import br.com.livresbs.livres.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,6 +19,12 @@ public class LojaController {
 
     @Autowired
     private LojaService lojaService;
+
+    @Autowired
+    private CarrinhoService carrinhoService;
+
+    @Autowired
+    private PedidoService pedidoService;
 
     @GetMapping("/produtos")
     public ProdutosDisponiveisDTO getProdutosDisponiveisVendaByCategoria (
@@ -26,6 +38,33 @@ public class LojaController {
 
         return lojaService.listarProdutosDisponiveisCompraConsumidor(cpf, numeroPagina, categorias);
 
+    }
+
+    @PostMapping("/carrinhos")
+    public void sincronizarProduto(
+            @RequestHeader(value = "cpf") String cpf,
+            @Valid @RequestBody ProdutoCarrinhoDTO produtoCarrinhoDTO
+    ) {
+        carrinhoService.sincronizarProduto(
+                cpf,
+                produtoCarrinhoDTO.getEstoqueProdutorId(),
+                produtoCarrinhoDTO.getQuantidade()
+        ) ;
+    }
+
+    @GetMapping("/carrinhos")
+    public CarrinhoDTO listarCarrinhos(
+            @RequestHeader(value = "cpf") String cpf
+    ) {
+
+        CarrinhoDTO carrinhoDTO = carrinhoService.listarCarrinhos(cpf);
+        return carrinhoDTO;
+
+    }
+
+    @GetMapping("/pedidos")
+    public CheckoutDTO checkout(@RequestHeader("cpf") String cpf) {
+        return pedidoService.checkout(cpf);
     }
 
 }
