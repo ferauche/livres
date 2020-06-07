13483b5427a6a2f646dac9b895c36db7d12e6aa6
DanielHGimenez <danielhgimenez027@gmail.com>
2020-06-06 21:06:06 -0300

    Revert "correção para usar tempo em UTC"
    This reverts commit 9db7f4e01d7b595c2eddc6b7790b7c88b8b23bc4.

--- backend/src/main/java/br/com/livresbs/livres/scheduler/VerificarDatasEntrega.java
+++ backend/src/main/java/br/com/livresbs/livres/scheduler/VerificarDatasEntrega.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Clock;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -24,7 +23,7 @@ public class VerificarDatasEntrega {
 
         List<DataEntrega> datasEntrega = dataEntregaRepository.findByStatus(StatusDataEntrega.ATIVA);
 
-        LocalDate dataAtual = LocalDate.now(Clock.systemUTC());
+        LocalDate dataAtual = LocalDate.now();
 
         for (DataEntrega dataEntrega : datasEntrega) {
 
