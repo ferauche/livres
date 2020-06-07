9cc5c43dd8efabdeff7209837bd8ecab02cb62e1
Edrei <edreifernandes@gmail.com>
2020-06-06 22:23:29 -0300

    Transformando components .js em componentes .vue

---
+++ frontend/src/components/CadPreComunidade.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row">
+    <div class="col-6 offset-md-3">
+      <div class="card">
+        <div class="card-header">
+          Cadastro de Consumidores
+        </div>
+        <div class="card-body">
+          <form>
+            <div class="md-form">
+              <input
+                autocomplete="off"
+                type="email"
+                id="nome"
+                class="form-control"
+                v-model.trim="nome"
+              />
+              <label for="nome">Nome</label>
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary pull-right"
+              @click="cadastrar()"
+            >
+              <i class="fa fa-floppy-o"></i> Cadastrar
+            </button>
+            <pre>Data: {{ $data }}</pre>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "",
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/ComMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="main-menu">
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <router-link v-bind:to="item.href">
+          <i v-bind:class="item.icon"></i>
+          <span class="nav-text">
+            {{ item.name }}
+          </span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: [
+        {
+          icon: "fa fa-hashtag fa-2x",
+          name: "Home",
+          href: "/",
+        },
+        {
+          icon: "fa fa-hashtag fa-2x",
+          name: "Cad Precomunidade",
+          href: "/cadprecomunidade",
+        },
+        {
+          icon: "fa fa-hashtag fa-2x",
+          name: "Cad Consumidor",
+          href: "/cadconsumidor",
+        },
+        {
+          icon: "fa fa-hashtag fa-2x",
+          name: "Produtos",
+          href: "/produtos",
+        },
+        {
+          icon: "fa fa-hashtag fa-2x",
+          name: "Checkout",
+          href: "/checkout",
+        },
+      ],
+    };
+  },
+};
+</script>
