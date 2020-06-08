141115006c127ba6a951abd93a9c6d5e6c663b39
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-08 16:40:22 -0300

    removendo /api a mais das rotas

--- backend/src/main/java/br/com/livresbs/livres/controller/ConsumidorController.java
+++ backend/src/main/java/br/com/livresbs/livres/controller/ConsumidorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping
 public class ConsumidorController {
     @Autowired
     ConsumidorImpl cons;
