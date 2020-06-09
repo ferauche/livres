722239a8f86a20214d0f1df32f78e950400d971a
Esdras Nani <esdraslnani@hotmail.com>
2020-06-08 23:34:45 -0300

    Delete Precomunidade

--- backend/src/main/java/br/com/livresbs/livres/controller/PreComunidadeController.java
+++ backend/src/main/java/br/com/livresbs/livres/controller/PreComunidadeController.java
@@ -3,45 +3,43 @@ package br.com.livresbs.livres.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.livresbs.livres.model.PreComunidade;
 import br.com.livresbs.livres.service.impl.PreComunidadeImpl;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "precomunidade")
 public class PreComunidadeController {
     @Autowired
     PreComunidadeImpl pcr;
 
     @CrossOrigin
-    @GetMapping("/precomunidade")
+    @GetMapping
     public List<PreComunidade> listaPreComunidades() {
         return pcr.listaPreComunidades();
     }
 
     @CrossOrigin
-    @GetMapping("/precomunidade/{id}")
+    @GetMapping("/{id}")
     public PreComunidade listaPreComunidadeUnica(@PathVariable(value = "id") long id) {
         return pcr.listaPreComunidadeUnica(id);
     }
 
     @CrossOrigin
-    @PostMapping("/precomunidade")
+    @PostMapping
     public PreComunidade cadastraPreComunidade(@RequestBody PreComunidade pc) {
         return pcr.cadastraPreComunidade(pc);
     }
     
     @CrossOrigin
-    @PutMapping("/precomunidade")
+    @PutMapping
     public PreComunidade editaPreComunidade(@RequestBody PreComunidade pc) {
     	return pcr.editaPreComunidade(pc);
     }
+
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluiPreComunidade(@PathVariable(value = "id") long id) { return pcr.excluiPreComunidade(id); }
 }
